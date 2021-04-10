cca6a405f4c73c085c08c697949c0c97bd2d0dd7
staffolis <simone.staffoli@locatee.ch>
2021-04-10 16:51:38 +0200

    init sort array app

---
+++ sortArray/src/main/java/sortArray/SortArrayApplication.java
@@ -0,0 +1,13 @@
+package sortArray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortArrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortArrayApplication.class, args);
+	}
+
+}
