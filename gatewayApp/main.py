670a6efa323df5a208f33b1dffae87c64724496e
Andrea <andreapinchera.1989@gmail.com>
2022-02-17 22:56:24 +0100

    feat: DEV-26 first implementation of retry back off

--- gatewayApp/main.py
+++ gatewayApp/main.py
@@ -1,25 +1,43 @@
+import http
 import time
 import post
 import generator
 import logger
-
+import math
 
 log = logger.Log(__name__)
 
-HOST = 'http://sortarray'
-PORT = '8080'
-URL = '/api/v1/array/default'
+config = dict(
+         HOST = 'http://127.0.0.1',
+         PORT = '8080',
+         URL = '/api/v1/array/default',
+         HEALTH = '/health'
+)
 
+def retry_back_off(attempt):
+    wait = math.pow(2, attempt)
+    next = math.pow(2, attempt+1)
+    time.sleep(wait)
+    log.logger.warning("Next connection attempt will be performed in {} seconds".format(next))
 
 def main():
-
-    while True:
+    for i in range(10):
         array = generator.generate_array_of_random_integers()
         pay_load = {'elements': array}
-        result = post.post_request('{}:{}{}'.format(HOST, PORT, URL), pay_load)
-        log.logger.debug(f'posted {array}')
-        log.logger.info(f'response from server: {result}')
-        time.sleep(1)
+
+        counter = 0
+        
+        err = post.request_health('{}:{}{}'.format(config['HOST'], config['PORT'], config['HEALTH']))
+        while not err:
+            retry_back_off(counter)
+            err = post.request_health('{}:{}{}'.format(config['HOST'], config['PORT'], config['HEALTH']))
+            counter += 1
+        try:
+            result = post.post_request('{}:{}{}'.format(config['HOST'], config['PORT'], config['URL']), pay_load)
+            log.logger.info('Shipped array: {}'.format(pay_load['elements']))
+            time.sleep(1)
+        except:
+            log.logger.error('Remote end closed connection without response')
 
 
 main()
--- gatewayApp/post.py
+++ gatewayApp/post.py
@@ -3,7 +3,14 @@ import logger
 
 log = logger.Log(__name__)
 
+def request_health(path):
+    try:
+        r = requests.get(path)
+    except requests.exceptions.RequestException as e:
+        return 
+    return r
 
 def post_request(path, pload):
     log.logger.debug('post is posting')
-    return requests.post(path, json=pload)
+    response = requests.post(path, json=pload)
+    return response
