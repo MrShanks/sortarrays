abd9324c5551e402b09fb155e110483b002d5c15
Andrea <andreapinchera.1989@gmail.com>
2022-02-21 14:10:13 +0100

    feat: DEV-26 revert back docker-compose, optimize format query

--- gatewayApp/main.py
+++ gatewayApp/main.py
@@ -8,8 +8,9 @@ import math
 log = logger.Log(__name__)
 
 config = dict(
-         HOST = 'http://sortarray',
+         HOST = 'http://127.0.0.1',
          PORT = '8080',
+         ENDPOINT = '',
          URL = '/api/v1/array/default',
          HEALTH = '/health'
 )
@@ -20,20 +21,24 @@ def retry_back_off(attempt):
     time.sleep(wait)
     log.logger.warning("Next connection attempt will be performed in {} seconds".format(next))
 
+def query_string_format(HOST = config['HOST'], PORT = config['PORT'], ENDPOINT = config['ENDPOINT']):
+    return '{host}:{port}{endpoint}'.format(host = HOST, port = PORT, endpoint = ENDPOINT)
+    
 def main():
-    while True:
+     while True:
         array = generator.generate_array_of_random_integers()
         pay_load = {'elements': array}
 
+        print()
         counter = 0
         
-        err = post.request_health('{}:{}{}'.format(config['HOST'], config['PORT'], config['HEALTH']))
+        err = post.request_health(query_string_format(ENDPOINT = config['HEALTH']))
         while not err:
             retry_back_off(counter)
-            err = post.request_health('{}:{}{}'.format(config['HOST'], config['PORT'], config['HEALTH']))
+            err = post.request_health(query_string_format(ENDPOINT = config['HEALTH']))
             counter += 1
         try:
-            result = post.post_request('{}:{}{}'.format(config['HOST'], config['PORT'], config['URL']), pay_load)
+            result = post.post_request(query_string_format(ENDPOINT = config['URL']), pay_load)
             log.logger.info('Shipped array: {}'.format(pay_load['elements']))
             time.sleep(1)
         except:
