6cdbdc5edfc655d75bf99c7937fb248e2a44e570
MrShanks <simonestaffoli@gmail.com>
2022-04-28 22:49:28 +0200

    feat: DEV-37 add status code to differentiate cases

--- sortArray/jwtauth/signup.go
+++ sortArray/jwtauth/signup.go
@@ -19,24 +19,27 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 
 	if user.Username == "" || user.Password == "" {
 		log.Println("Username or Password is empty, that cannot be tolerated")
+		w.WriteHeader(http.StatusForbidden)
 		return
 	}
 
-	CreateUser(&user)
+	status := CreateUser(&user)
+	w.WriteHeader(status)
 }
 
-func CreateUser(user *model.User) {
+func CreateUser(user *model.User) int {
 
 	var userCheck model.User
 	database.Connector.Where("username = ?", user.Username).First(&userCheck)
 	if userCheck.Username != "" {
 		log.Println(fmt.Sprintf("User: %s already exists, please chose another one", user.Username))
-		return
+		return http.StatusForbidden
 	}
 
 	hashedBytePassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
 	if err != nil {
 		log.Println(fmt.Sprintf("Error occured while hashing: %v", err))
+		return http.StatusInternalServerError
 	}
 
 	newUser := model.User{
@@ -45,4 +48,5 @@ func CreateUser(user *model.User) {
 	}
 	database.Connector.Save(newUser)
 	log.Println(fmt.Sprintf("User : %s has been created", user.Username))
+	return http.StatusCreated
 }
